954c2cf8a5af3c77df770992aab79412c0161ea1
Oleg Zhmaev <grasword6@gmail.com>
2022-08-10 02:20:13 +0300

    refactoring for tests suite

--- tests/todo.test.js
+++ tests/todo.test.js
@@ -1,33 +1,40 @@
-import ToDoModel from '../model/todoModel'
+import Client from '../model/todoModel'
 
 describe('Todo', () => {
   test('remote API returns 200', async () => {
-    const { status } = await ToDoModel.fetch()
+    const { status } = await Client.fetch('todos/1')
     expect(status).toBe(200)
   })
 
+  test('remote API returns 404 on nonexisting id', async () => {
+    const { status } = await Client.fetch('todos/201')
+    expect(status).toBe(404)
+  })
+
   test('return data is an object', async () => {
-    const { data } = await ToDoModel.fetch()
+    const { data } = await Client.fetch('todos/1')
     expect(data).toMatchSnapshot()
   })
 
-  test('formatted todo complies to requirements', async () => {
-    const response = await ToDoModel.fetch()
-    const todo = ToDoModel.formatTodo(response.data)
-    expect(todo).toMatchSnapshot()
-  })
-
   test('completed should be boolean and title should be string', async () => {
-    const response = await ToDoModel.fetch()
-    const todo = ToDoModel.formatTodo(response.data)
-    todo.forEach((t) => {
-      expect(typeof t.title).toBe('string')
-      expect(typeof t.completed).toBe('boolean')
-    })
+    const response = await Client.fetch('todos/1')
+    const todo = Client.formatTodo(response.data)
+    expect(todo).toEqual(
+      expect.objectContaining({
+        completed: expect.any(Boolean),
+        title: expect.any(String)
+      })
+    )
   })
 
   test('returns 120 items', async () => {
-    const { data } = await ToDoModel.fetch(120)
+    const { data } = await Client.fetch('todos', 120)
     expect(data.length).toBe(120)
+  })
+
+  // TODO returns 404
+  test('returns 404 with invalid range of todos', async () => {
+    const { status } = await Client.fetch('todos', 201)
+    expect(status).toBe(404)
   })
 })
