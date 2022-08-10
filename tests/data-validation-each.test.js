027dc51231bcc1ec34a87390d30fc4456db4cb24
Oleg Zhmaev <grasword6@gmail.com>
2022-08-10 22:59:03 +0300

    implement data validation tests using `test.each` approach

---
+++ tests/data-validation-each.test.js
@@ -0,0 +1,13 @@
+import Client from '../model/todoModel'
+
+const ID_LIST = Array.from({ length: 210 }, (_, i) => i + 1)
+
+describe('Dataset', () => {
+  test.each(ID_LIST)('todos/%d complies to requirements', async (id) => {
+    const { status, data } = await Client.fetch(`todos/${id}`)
+
+    expect(status).toBe(200)
+    expect(data.constructor.name).toBe('Object')
+    expect(data.title.length).toBeGreaterThan(20)
+  })
+})
