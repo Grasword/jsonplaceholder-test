9874bd6beacb9aab0e50d6a35d37a3b02254b8d6
Oleg Zhmaev <grasword6@gmail.com>
2022-08-10 17:18:16 +0300

    refactor tests to use property matchers on snapshot

--- tests/todo.test.js
+++ tests/todo.test.js
@@ -11,30 +11,18 @@ describe('Todo', () => {
     expect(status).toBe(404)
   })
 
-  test('return data is an object', async () => {
-    const { data } = await Client.fetch('todos/1')
-    expect(data).toMatchSnapshot()
-  })
-
-  test('completed should be boolean and title should be string', async () => {
+  test('completed property should be boolean and title should be string', async () => {
     const response = await Client.fetch('todos/1')
     const todo = Client.formatTodo(response.data)
-    expect(todo).toEqual(
-      expect.objectContaining({
-        completed: expect.any(Boolean),
-        title: expect.any(String)
-      })
-    )
+    expect(todo).toMatchSnapshot({
+      completed: expect.any(Boolean),
+      title: expect.any(String)
+    })
   })
 
-  test('returns 120 items', async () => {
-    const { data } = await Client.fetch('todos', 120)
-    expect(data.length).toBe(120)
-  })
-
-  // TODO returns 404
-  test('returns 404 with invalid range of todos', async () => {
-    const { status } = await Client.fetch('todos', 201)
-    expect(status).toBe(404)
+  test('limit return data to 120 items', async () => {
+    const { data } = await Client.fetch('todos')
+    const slicedData = Client.limit(data, 0, 120)
+    expect(slicedData.length).toBe(120)
   })
 })
